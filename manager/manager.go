4229e7ea3801b342318d61e2d69766ac73ba5241
Mohak <mohaknainmalhotra@gmail.com>
2024-09-07 21:57:43 +1000

    skeleton code

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,28 @@
 package manager
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	. "github.com/mnm458/gorchestrator/task"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string][]Task
+	EventDb       map[string][]TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+
+}
+
+func (m *Manager) UpdateTasks() {
+
+}
+
+func (m *Manager) SendWork() {
+
+}
--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,29 @@
 package worker
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/mnm458/gorchestrator/task"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]task.Task
+	TaskCount int
+}
+
+func (w *Worker) RunTask() error {
+	return nil
+}
+
+func (w *Worker) StartTask() {
+}
+
+func (w *Worker) StopTask() {
+
+}
+
+func (w *Worker) CollectStats() {
+
+}
