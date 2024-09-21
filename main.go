9f6825ac09becae6a4bbca859c549da770589545
Mohak <mohaknainmalhotra@gmail.com>
2024-09-21 20:40:40 +1000

    test: worker setup

--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
-	"github.com/mnm458/gorchestrator/manager"
-	"github.com/mnm458/gorchestrator/node"
 	"github.com/mnm458/gorchestrator/task"
 	"github.com/mnm458/gorchestrator/worker"
 )
@@ -84,61 +81,89 @@ func stopContainer(d *task.Docker) *task.DockerResult {
 	}
 }
 func main() {
-	t := task.Task{
-		ID:     uuid.New(),
-		Name:   "Task-1",
-		State:  task.Pending,
-		Image:  "Image-1",
-		Memory: 1024,
-		Disk:   1,
-	}
-	te := task.TaskEvent{
-		ID:        uuid.New(),
-		State:     task.Pending,
-		Timestamp: time.Now(),
-		Task:      t,
-	}
-	fmt.Printf("task: %v\n", t)
-	fmt.Printf("task event: %v\n", te)
+	// t := task.Task{
+	// 	ID:     uuid.New(),
+	// 	Name:   "Task-1",
+	// 	State:  task.Pending,
+	// 	Image:  "Image-1",
+	// 	Memory: 1024,
+	// 	Disk:   1,
+	// }
+	// te := task.TaskEvent{
+	// 	ID:        uuid.New(),
+	// 	State:     task.Pending,
+	// 	Timestamp: time.Now(),
+	// 	Task:      t,
+	// }
+	// fmt.Printf("task: %v\n", t)
+	// fmt.Printf("task event: %v\n", te)
 
+	// w := worker.Worker{
+	// 	Queue: *queue.New(),
+	// 	Db:    make(map[uuid.UUID]task.Task),
+	// }
+	// fmt.Printf("worker: %v\n", w)
+	// w.CollectStats()
+	// w.RunTask()
+	// w.StartTask()
+	// w.StopTask()
+	// m := manager.Manager{
+	// 	Pending: *queue.New(),
+	// 	TaskDb:  make(map[string][]task.Task),
+	// 	EventDb: make(map[string][]task.TaskEvent),
+	// 	Workers: []string{w.Name},
+	// }
+	// fmt.Printf("manager: %v\n", m)
+	// m.SelectWorker()
+	// m.UpdateTasks()
+	// m.SendWork()
+
+	// n := node.Node{
+	// 	Name:   "Name-1",
+	// 	Ip:     "192.168.1.1",
+	// 	Cores:  4,
+	// 	Memory: 1024,
+	// 	Disk:   25,
+	// 	Role:   "worker",
+	// }
+	// fmt.Printf("node: %v\n", n)
+
+	// fmt.Println("create a test container")
+	// dockerTask, createResult := createContainer()
+	// if createResult.Error != nil {
+	// 	fmt.Println(createResult.Error)
+	// 	os.Exit(1)
+	// }
+	// time.Sleep(time.Second * 5)
+	// fmt.Printf("stopping container %s\n", createResult.ContainerId)
+	// _ = stopContainer(dockerTask)
+	db := make(map[uuid.UUID]*task.Task)
 	w := worker.Worker{
 		Queue: *queue.New(),
-		Db:    make(map[uuid.UUID]task.Task),
+		Db:    db,
 	}
-	fmt.Printf("worker: %v\n", w)
-	w.CollectStats()
-	w.RunTask()
-	w.StartTask()
-	w.StopTask()
-	m := manager.Manager{
-		Pending: *queue.New(),
-		TaskDb:  make(map[string][]task.Task),
-		EventDb: make(map[string][]task.TaskEvent),
-		Workers: []string{w.Name},
+	t := task.Task{
+		ID:    uuid.New(),
+		Name:  "test-container-1",
+		State: task.Scheduled,
+		Image: "strm/helloworld-http",
 	}
-	fmt.Printf("manager: %v\n", m)
-	m.SelectWorker()
-	m.UpdateTasks()
-	m.SendWork()
-
-	n := node.Node{
-		Name:   "Name-1",
-		Ip:     "192.168.1.1",
-		Cores:  4,
-		Memory: 1024,
-		Disk:   25,
-		Role:   "worker",
+	// first time the worker will see the task
+	fmt.Println("starting task")
+	w.AddTask(t)
+	result := w.RunTask()
+	if result.Error != nil {
+		panic(result.Error)
 	}
-	fmt.Printf("node: %v\n", n)
-
-	fmt.Println("create a test container")
-	dockerTask, createResult := createContainer()
-	if createResult.Error != nil {
-		fmt.Println(createResult.Error)
-		os.Exit(1)
+	t.ContainerID = result.ContainerId
+	fmt.Printf("task %s is running in container %s\n", t.ID, t.ContainerID)
+	fmt.Println("Sleepy time")
+	time.Sleep(time.Second * 30)
+	fmt.Printf("stopping task %s\n", t.ID)
+	t.State = task.Completed
+	w.AddTask(t)
+	result = w.RunTask()
+	if result.Error != nil {
+		panic(result.Error)
 	}
-	time.Sleep(time.Second * 5)
-	fmt.Printf("stopping container %s\n", createResult.ContainerId)
-	_ = stopContainer(dockerTask)
-
 }
